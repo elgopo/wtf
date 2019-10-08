bc5de6b051a7ee6ad3240091f2f827f3b7072b4d
mks <elgopo94@gmail.com>
2019-10-08 17:14:37 +0300

    проект

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Страница входа";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Страница для отображения новостей";
+
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Страница для отображения профиля";
+
+            return View();
+        }
+    }
+}
